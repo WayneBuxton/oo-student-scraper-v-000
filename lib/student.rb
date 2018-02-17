8ee80c74c9e986970b5c4bd83c0c7548835c505a
Wayne Buxton <waynebuxton2184@yahoo.com>
2018-02-17 22:13:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,6 @@ class Student
 
     def self.create_from_collection(students_array)
       students_array.each { |student| Student.new(student) }
-        #binding.pry
     end
 
   def add_student_attributes(attributes_hash)
